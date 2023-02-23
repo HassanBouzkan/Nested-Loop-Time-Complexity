99d8f423b5db7af1e685740c3384708a87255f6a
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-02-23 20:09:58 +0100

    create nested_loop.cpp

---
+++ nested_loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int n = 10;
+    int x = 0;
+    int s = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            for (int k = 1; k < n; ++k) {
+                ++x;
+                s += x;
+            }
+        }
+    }
+
+    std::cout << "s = " << s << std::endl;
+
+    return 0;
+}
